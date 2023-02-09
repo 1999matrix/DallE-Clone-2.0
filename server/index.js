86605fc3b178588ca70add9c06b95790304e4a39
1999matrix <shashwatsd44@gmail.com>
2023-02-09 20:59:15 +0530

    .

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-const path = require('path')
+// const path = require('path')
+import * as path from 'path'
 
 dotenv.config();
 
